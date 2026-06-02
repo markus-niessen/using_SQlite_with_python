3ffb152383e052b07ea065f632d1b1e7e87287ca
Markus Nießen <markusniessen1@gmail.com>
2026-06-02 16:27:06 +0200

    Added SQL query for simple_books

--- step1.py
+++ step1.py
@@ -4,7 +4,7 @@ engine = create_engine('sqlite:///data/textbook.sqlite3')
 
 with engine.connect() as connection:
     # Run an SQL query
-    results = connection.execute(text('SELECT * FROM table_name'))
+    results = connection.execute(text('SELECT * FROM simple_books'))
     rows = results.fetchall()
 
     # Print results
