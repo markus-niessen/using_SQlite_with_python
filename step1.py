a227873235ec8bfd1790c8c6f0da48fd60ec3e26
markus-niessen <markusniessen1@gmail.com>
2026-06-02 16:12:59 +0200

    Initial commit

---
+++ step1.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine, text
+
+engine = create_engine('sqlite:///data/textbook.sqlite3')
+
+with engine.connect() as connection:
+    # Run an SQL query
+    results = connection.execute(text('SELECT * FROM table_name'))
+    rows = results.fetchall()
+
+    # Print results
+    for row in rows:
+        print(row)
